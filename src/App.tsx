557c01394da6e1223660d5125b707219c67e89ab
unknown <paatatepnadze1@gmail.com>
2022-10-27 14:50:38 +0200

    NetShop React Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, {createElement as e, useState} from 'react';
+import './App.css';
+import {Product} from './components/product'
+import {products} from './data/products'
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-2xl pt-5" >
+      <Product product={ products[0]}/>
+      <Product product={ products[1]}/>
+    </div>
+  );
+}
+
+export default App;
