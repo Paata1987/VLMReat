6a52fcb452cda1dffff3b371d51750c8b5ea39b4
unknown <paatatepnadze1@gmail.com>
2022-10-28 13:18:52 +0200

    add Product Show Hide component state

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IProduct } from '../models'
 
 interface ProductProps {
@@ -6,12 +6,22 @@ interface ProductProps {
 }
 
 export function Product({product}:ProductProps){
-    
+    const [details, setDetails] = useState(false)
     return(
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">Product:
         <img src={product.image} className="w-1/6" alt={product.title} />
         <p>{product.title}</p>
-        <span className="font-bold">{product.price}</span>
+        <p className="font-bold">{product.price}</p>
+        <button className="py-2 px-4 border bg-yellow-400"
+         onClick={ ()=>setDetails(prev => !prev) }
+         >
+            {details ? 'Hide Deteils' : 'Show Details'}
+         </button>
+         {details && <div>
+            <p>
+                {product.description}
+            </p>
+         </div>}
         </div>
     )
 }
