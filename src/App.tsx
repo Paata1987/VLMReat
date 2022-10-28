65a739e4b27a80c4351a9b34cc8dc2936c742d91
unknown <paatatepnadze1@gmail.com>
2022-10-28 13:59:48 +0200

    create dataa loader/components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
+import axios from 'axios';
 import React, {createElement as e, useEffect, useState} from 'react';
 import './App.css';
 import {Product} from './components/product'
 import {products} from './data/products'
+import { IProduct } from './models';
 
 function App() {
 
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  const [loading, setLoading] = useState(false)
+
+
+
+//Asynch fetch /Get data 
+ async function fetchProducts() {
+   setLoading(true)
+   const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=10')
+   setProducts(response.data)
+   setLoading(false)
+ }
+
 
   useEffect(()=>{
-    console.log('effect')
+    fetchProducts();
   }, [])
 
 
 
   return (
     <div className="container mx-auto max-w-2xl pt-5" >
-
+      {loading && <p className="text-ceneter">Loading....</p>}
       { products.map(product => <Product product={product} key={product.id} /> )}
 
       {/* <Product product={ products[0]}/>
